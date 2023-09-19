ca21deef406dce1feebac9339dcff02e127eeea6
Red <kauawar04@gmail.com>
2023-09-19 18:40:36 -0300

    animation after 'desenvolvedor front-end'

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const place = document.querySelectorAll(".placeholder");
+const li = document.querySelectorAll(".header a");
 
 function handleClick(event) {
   event.currentTarget.classList.toggle("active");
+  event.currentTarget.classList.remove("detail");
 }
+
+function clickButton(event) {
+  event.preventDefault();
+  switch (event.currentTarget.innerHTML) {
+    case "Skills":
+      document.querySelector(".skills").classList.toggle("detail");
+      document.querySelector(".skills").classList.toggle("active");
+      break;
+    case "Laboratório":
+      document.querySelector(".lab").classList.toggle("detail");
+      document.querySelector(".lab").classList.toggle("active");
+  }
+}
+
+li.forEach((item) => {
+  item.addEventListener("click", clickButton);
+});
 
 place.forEach((item) => {
   item.addEventListener("click", handleClick);
